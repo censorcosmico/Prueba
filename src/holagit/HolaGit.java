51e2de4feb895a04eba5cd78c880a9ad1a8c9ca2
Cosmico <Censor@COSMICO>
2019-01-02 17:01:35 +0100

    Añadido nueva linea de codigo

--- src/holagit/HolaGit.java
+++ src/holagit/HolaGit.java
@@ -5,6 +5,7 @@ public class HolaGit {
     public static void main(String[] args) {
         System.out.println("Esto es una prueba");
         System.out.println("");
+        System.out.println("patata de mierda");
     }
 
 }
